9c5661fac02f213ba36ff9c2f80371f5231b8a5d
Mannzil <brianmannzil@gmail.com>
2026-08-12 14:38:03 -0700

    Practice Python Variables
    Tested out tuples, sets, lists and checking data types

---
+++ main.py
@@ -0,0 +1,26 @@
+my_tuple_var = (88, 0.5,'Yuta')
+print( 'Tuple: ', my_tuple_var)
+my_integer_var = 10
+print( 'integer:' , my_integer_var)
+my_float_var = 99.9
+print( 'float:' , my_float_var)
+my_string_var = 'Luffy x Ace'
+print( 'string:' , my_string_var)
+my_boolean_var = True
+print( 'boolean:' , my_boolean_var)
+my_set_var = {7, 3.5, 'Sukuna'}
+print( 'set:' , my_set_var)
+my_dictionary_var = {4, 3.5, 'Zoro', 'Saturo Gojo'}
+print( 'dictionary:' , my_dictionary_var)
+my_range_var = range(7)
+print( 'range:', my_range_var)
+my_list_var = [8, 9.9, 'jjk']
+print( 'list:' , my_list_var)
+my_none_var = None
+print( 'None:' , my_none_var)
+my_integer_var = 10
+print(type(my_integer_var))
+developer = 'Mannzil'
+print(type(developer))
+Super_Saiyan = 'Goku'
+print(isinstance(Super_Saiyan, str))
