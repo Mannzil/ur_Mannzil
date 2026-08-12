60454813b4413dfd7740b6ed43eea9e808fc7330
Mannzil <brianmannzil@gmail.com>
2026-08-12 16:29:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,30 @@ print(type(my_integer_var))
 developer = 'Mannzil'
 print(type(developer))
 
+#     String Practice
+
+my_str = 'Hello World'
+print('World' in my_str) 
+print('z' in my_str)
+my_str = 'One Piece'
+print(len(my_str))
+print(my_str[4])
+my_str_1 = "Luffy is "
+my_str_2 = 'King of the Pirates'
+str_plus_str = my_str_1 + '' +my_str_2 
+print(str_plus_str)
+sound = 'Arrrgh'
+repeated_sound = sound * 4
+print(repeated_sound)
+name = 'Ace'
+Age = 20
+name_and_Age = name + str (Age)
+print(name_and_Age)
+name = 'Kaneki'
+age = 19
+name_and_age = f'My name is {name} and I am {age} years old.'
+print(name_and_age)
+str = 'Bankai'
+print(str[::-1])
+
 
