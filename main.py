8b21e6edf7a436ec60c3b71819757a22a0ab2b9b
Mannzil <brianmannzil@gmail.com>
2026-08-13 14:55:34 -0700

    Employee Profile Generator

--- main.py
+++ main.py
@@ -71,8 +71,18 @@ print(employee_info)
 experience_years = 5
 experience_info = 'Experience: ' + str(experience_years) + ' years'
 print(experience_info)
-
-employee_card = f'Employee: {full_name} | Age: {employee_age}'
-
+position = 'Data Analyst'
+salary = 75000
+employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
+print(employee_card)
+employee_code = 'DEV-2026-JD-001'
+department = employee_code[0:3]
+print(department)
+year_code = employee_code[4:8]
+print(year_code)
+initials = employee_code[9:11]
+print(initials)
+last_three = (employee_code[-3:])
+print(last_three)
 
 
